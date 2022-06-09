88290cd896b8f1b6d807d4ec9c415f08d0588039
59114_Chetana <deslechetna1999@gmail.com>
2022-06-10 00:47:52 +0530

    [Chetana] Added : Create DataTables

--- ProductReviewManagementUsingLINQ/Program.cs
+++ ProductReviewManagementUsingLINQ/Program.cs
@@ -34,7 +34,7 @@ class Program
         bool end = true;
         while (end)
         {
-            Console.WriteLine("\nSelect Option\n1.CreateProductReview\n 2.Retrive Top 3 Records \n 3.Retrive Top 3 Record With Product Id \n 4.Get Top Records Count With ProductId \n 5.Retrieve Only ProductId And Review \n 6. Skip Top 5 Records \n  7.End Of Program");
+            Console.WriteLine("\nSelect Option\n1.CreateProductReview\n 2.Retrive Top 3 Records \n 3.Retrive Top 3 Record With Product Id \n 4.Get Top Records Count With ProductId \n 5.Retrieve Only ProductId And Review \n 6. Skip Top 5 Records \n7.Create DataTable \n 8.End Of Program");
             Console.WriteLine("\nEnter Option For Exicute The Program");
             int option = Convert.ToInt16(Console.ReadLine());
             Operations operations = new Operations();
@@ -59,6 +59,9 @@ class Program
                     operations.SkipTop5Records(reviewList);
                     break;
                 case 7:
+                    operations.Datatables();
+                    break;
+                case 8:
                     end = false;
                     break;
                 default:
