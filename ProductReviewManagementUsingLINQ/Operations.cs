5ea2eb35d416a8c79929346e29ac98532931e797
59114_Chetana <deslechetna1999@gmail.com>
2022-06-09 23:17:00 +0530

    [Chetana] Added : Retrieve Top Three Records

---
+++ ProductReviewManagementUsingLINQ/Operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementUsingLINQ
+{
+    public class Operations
+    {
+        public void GetTop3Records(List<ProductReview> reviewlist)
+        {
+            var result = reviewlist.OrderByDescending(x => x.Rating).Take(3).ToList();
+            Display(result);
+        }
+        public void Display(List<ProductReview> list)
+        {
+            //Display list
+            foreach (var product in list)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine(product.ProductId + " | " + product.UserId + " | " + product.Rating + " | " + product.Review + " | " + product.IsLike);
+            }
+        }
+    }
+}
